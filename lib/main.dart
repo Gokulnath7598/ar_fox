3e44f78443ce44d322b5d864beeb18dbb23bf1bc
Gokulnath <gokulnath.gopu@rootquotient.com>
2022-10-28 18:32:37 +0530

    temp commit.

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,19 @@
+import 'dart:io';
+import 'dart:math';
+
 import 'package:ar_flutter_plugin/ar_flutter_plugin.dart';
+import 'package:ar_flutter_plugin/datatypes/config_planedetection.dart';
+import 'package:ar_flutter_plugin/datatypes/node_types.dart';
+import 'package:ar_flutter_plugin/managers/ar_anchor_manager.dart';
+import 'package:ar_flutter_plugin/managers/ar_location_manager.dart';
+import 'package:ar_flutter_plugin/managers/ar_object_manager.dart';
+import 'package:ar_flutter_plugin/managers/ar_session_manager.dart';
+import 'package:ar_flutter_plugin/models/ar_node.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'dart:async';
 import 'package:flutter/services.dart';
-import 'debugoptionsexample.dart';
+import 'package:vector_math/vector_math_64.dart';
 import 'placeafox.dart';
 
 void main() {
@@ -60,7 +71,7 @@ class MyHome extends StatelessWidget {
       body: ListView(children: [
         ListTile(onTap: (){
           Navigator.push(context,
-              MaterialPageRoute(builder: (context) => const DebugOptionsWidget()));
+              MaterialPageRoute(builder: (context) => LocalAndWebObjectsWidget()));
         },title: const Text('Options'),subtitle: const Text('Enable options to see planes, coordinates, points'),
         ),
         ListTile(onTap: () {
@@ -72,5 +83,88 @@ class MyHome extends StatelessWidget {
         )
       ]),
     );
+  }
+}
+
+
+
+class LocalAndWebObjectsWidget extends StatefulWidget {
+  LocalAndWebObjectsWidget({Key key}) : super(key: key);
+  @override
+  _LocalAndWebObjectsWidgetState createState() =>
+      _LocalAndWebObjectsWidgetState();
+}
+
+class _LocalAndWebObjectsWidgetState extends State<LocalAndWebObjectsWidget> {
+  ARSessionManager arSessionManager;
+  ARObjectManager arObjectManager;
+  //String localObjectReference;
+  ARNode localObjectNode;
+  //String webObjectReference;
+  ARNode webObjectNode;
+  ARNode fileSystemNode;
+  HttpClient httpClient;
+
+  @override
+  void dispose() {
+    super.dispose();
+    arSessionManager.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Local & Web Objects'),
+        ),
+        body: Container(
+            child: Stack(children: [
+              ARView(
+                onARViewCreated: onARViewCreated,
+                planeDetectionConfig: PlaneDetectionConfig.horizontalAndVertical,
+              ),
+              Align(
+                  alignment: FractionalOffset.bottomCenter,
+                  child:
+                  Column(mainAxisAlignment: MainAxisAlignment.end, children: [
+                    ElevatedButton(
+                        onPressed: onWebObjectAtOriginButtonPressed,
+                        child: Text("Add/Remove Web\nObject at Origin"))
+                  ]))
+            ])));
+  }
+
+  void onARViewCreated(
+      ARSessionManager arSessionManager,
+      ARObjectManager arObjectManager,
+      ARAnchorManager arAnchorManager,
+      ARLocationManager arLocationManager) {
+    this.arSessionManager = arSessionManager;
+    this.arObjectManager = arObjectManager;
+
+    this.arSessionManager.onInitialize(
+      showAnimatedGuide: false,
+      showFeaturePoints: false,
+      showPlanes: false,
+      showWorldOrigin: false,
+      handleTaps: false,
+    );
+    this.arObjectManager.onInitialize();
+  }
+
+  Future<void> onWebObjectAtOriginButtonPressed() async {
+    if (this.webObjectNode != null) {
+      this.arObjectManager.removeNode(this.webObjectNode);
+      this.webObjectNode = null;
+    } else {
+      var newNode = ARNode(
+          type: NodeType.webGLB,
+          uri:
+          "https://github.com/KhronosGroup/glTF-Sample-Models/raw/master/2.0/Duck/glTF-Binary/Duck.glb",
+          scale: Vector3(0.2, 0.2, 0.2),
+      position: Vector3(0, 0, -2),);
+      bool didAddWebNode = await this.arObjectManager.addNode(newNode);
+      this.webObjectNode = (didAddWebNode) ? newNode : null;
+    }
   }
 }
